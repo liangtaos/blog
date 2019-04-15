e98810feebe89a9d18c9746df070d25eb67a9b92
lt <cold@example.com>
2019-04-15 18:59:26 +0800

    class view

--- testidea/blog/views.py
+++ testidea/blog/views.py
@@ -1,15 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
 # Create your views here.
 
 from .models import Post,Tag, Category
 from config.models import Link, SideBar
 from comment.models import Comment
 
-from django.views.generic import ListView  # 对多个对象展示的应用，列表展示
-from django.views.generic import DetailView # 对单个对象展示的应用
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 
 class CommonMixin(object):
     def get_context_data(self, **kwargs):
+        category_id = self.kwargs.get('category_id')
+        tag_id = self.kwargs.get('tag_id')
         cates = Category.objects.all()
         showCate = []
         hiddenCate = []
@@ -18,54 +23,37 @@ class CommonMixin(object):
                 showCate.append(cate)
             else:
                 hiddenCate.append(cate)
-
+        context = super(CommonMixin, self).get_context_data(**kwargs)
         hotPosts = Post.objects.all()[:10]
         sidebar = SideBar.objects.all().order_by('display_type')
         comment = Comment.objects.all().order_by('created_time')[:5]
         extra_context = {
+            'category_id': category_id,
+            'tag_id': tag_id,
             'showCate': showCate,
             'hiddenCate': hiddenCate,
             'hotPosts': hotPosts,
             'sidebar': sidebar,
             'comment': comment,
         }
-        context = super(CommonMixin, self).get_context_data(**kwargs)
         context.update(extra_context)
         return context
 
 
-class BasePostView(CommonMixin, ListView):
+class BasePostView(CommonMixin, ListView,):
     model = Post
     template_name = 'list.html'
     context_object_name = 'posts'
     paginate_by = 4
 
-    # def get_context_data(self, **kwargs):
-    #     cates = Category.objects.all()
-    #     showCate = []
-    #     hiddenCate = []
-    #     for cate in cates:
-    #         if cate.is_nav:
-    #             showCate.append(cate)
-    #         else:
-    #             hiddenCate.append(cate)
-    #     context = super(BasePostView, self).get_context_data(**kwargs)
-    #     hotPosts = Post.objects.all()[:10]
-    #     sidebar = SideBar.objects.all().order_by('display_type')
-    #     comment = Comment.objects.all().order_by('created_time')[:5]
-    #     extra_context = {
-    #         'showCate': showCate,
-    #         'hiddenCate': hiddenCate,
-    #         'hotPosts': hotPosts,
-    #         'sidebar': sidebar,
-    #         'comment': comment,
-    #     }
-    #     context.update(extra_context)
-    #     return context
+
+
+
 
 
 class IndexView(BasePostView):
     pass
+
 
 
 class CategoryView(BasePostView):
@@ -86,9 +74,7 @@ class TagView(BasePostView):
         posts = tag.tags.all()
         return posts
 
-
 class PostView(CommonMixin,DetailView):
     model = Post
     template_name = 'detail.html'
     context_object_name = 'post'
-
